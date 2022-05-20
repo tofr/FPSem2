40bdb899f7dacb24e7aee583be7cce36b7e6e7e9
ArrtheFuchs <afuchs30@stuy.edu>
2022-05-20 13:21:03 -0400

    glad to be here

---
+++ Entity/Enemy.java
@@ -0,0 +1,10 @@
+public abstract class enemy{
+    
+    protected int health;
+    protected int attack;
+    protected boolean falling;
+    
+    public class attack(){};
+
+    public class move(){};
+}
