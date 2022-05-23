9e2bcdeef05154b5e7db18509d29d1fc0884657d
Jason Zhou <jzhou33@stuy.edu>
2022-05-23 13:20:49 -0400

    button

--- Handler.java
+++
@@ -1,6 +0,0 @@
-import java.awt.*;
-
-public abstract class Handler {
-    public abstract void draw(Graphics g, DriverRunner driver);
-    public abstract void tick(DriverRunner driver);
-}
---
+++ v2/Blocks/Button.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Button extends Block {
+
+    public Button(int X, int Y) {
+        super(X, Y);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
